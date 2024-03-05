dc4497cf5059550ec58675225148e83814874fae
zarnigor87 <153763391+zarnigor87@users.noreply.github.com>
2024-03-04 20:23:21 -0600

    added more files

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
